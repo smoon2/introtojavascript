5c4c94a0b2564cf27f37ee712f7a17ccfa910ed0
Sonie Moon <smoon2@uw.edu>
2017-05-17 21:44:20 -0500

    Initial Commit

---
+++ introToJS.js
@@ -0,0 +1,22 @@
+function display(){
+    document.write("Introduction to JavaScript");
+}
+
+
+
+function fizzBuzz(){
+    for(var i = 0; i <= 100; i++ ){
+
+        //if(i % 3 == 0){
+        //    console.log("Fizz");
+        //}
+        //else if(i % 5){
+        //    console.log("Buzz");
+        //}
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log("FizzBuzz");
+        }
+    }
+    
+    
+}
