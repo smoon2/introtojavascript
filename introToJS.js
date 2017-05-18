e518ca77962c7761daa6b940ab5f45884f1118c7
Sonie Moon <smoon2@uw.edu>
2017-05-17 21:51:09 -0500

    Added bCount function

--- introToJS.js
+++ introToJS.js
@@ -19,3 +19,14 @@ function fizzBuzz(){
     
     
 }
+
+function countBs(str){
+    var bCount = 0;
+    for(var i =0; i <= str.length; i++){
+        if (str.charAt(i) === "B"){
+            bCount++;
+        }
+        
+    }
+    return(bCount);
+}
